c6872e29006d0a54a5935445b5faf8928688598d
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2022-03-01 15:45:01 +0300

    js for mobile and pixel perfect

--- script/script.js
+++ script/script.js
@@ -1,11 +1,19 @@
-const prevArrow = document.querySelector(".arrow_prev");
-const nextArrow = document.querySelector(".arrow_next");
+const prevArrow = document.querySelector(".arrow_prev.d-sm-none");
+const nextArrow = document.querySelector(".arrow_next.d-sm-none");
 const slides = document.querySelectorAll(".carousel__item");
 const bikefilters = document.querySelectorAll(".bikes-list__item");
 const bikeSlides = document.querySelectorAll(".bikes-list__slide");
 const themeSwitcher = document.querySelector(".toggle");
+const menuLinks = document.querySelectorAll('.nav-item');
+const menuInput = document.querySelector('#menu-btn');
+const bikesOptions = document.querySelectorAll('.bikes-option');
+const selectBikeOptions = document.querySelector('.bikes-options');
+const prevArrowMobile = document.querySelector('.arrow_prev.d-md-none');
+const nextArrowMobile = document.querySelector('.arrow_next.d-md-none');
+const bikeDots = document.querySelectorAll('.bikes-slider-dot');
 
 let slideIndex = 1;
+let bikeSlideIndex = 1;
 
 function showSlide(index) {
   if (index > slides.length) {
@@ -19,9 +27,18 @@ function showSlide(index) {
   slides[slideIndex-1].classList.remove('hidden');
 }
 
-function setFilterState(element) {
+function showBikesSlide(index, nodes) {
+  nodes.forEach(element => {
+    element.classList.add('d-sm-none');
+  })
+  nodes[index].classList.remove('d-sm-none');
+}
+
+function setFilterState(element, options=false) {
   bikefilters.forEach(elem => elem.classList.remove('selected'));
-  element.classList.add('selected');
+  if (!options) {
+    element.classList.add('selected');
+  }
 }
 
 function filterBikes(id) {
@@ -34,12 +51,26 @@ function filterBikes(id) {
   })
 }
 
+function getCurrentTerrainSlides() {
+  const terrain = document.querySelector('.bikes-list__slide:not(.hidden)');
+  return terrain.querySelectorAll('.bike__item');
+}
+
 showSlide(slideIndex);
 prevArrow.addEventListener('click', () => {
   slides[slideIndex-1].classList.add('hidden');
   showSlide(slideIndex - 1);
 });
 nextArrow.addEventListener('click', () => {
+  slides[slideIndex-1].classList.add('hidden');
+  showSlide(slideIndex + 1);
+});
+
+prevArrowMobile.addEventListener('click', () => {
+  slides[slideIndex-1].classList.add('hidden');
+  showSlide(slideIndex - 1);
+});
+nextArrowMobile.addEventListener('click', () => {
   slides[slideIndex-1].classList.add('hidden');
   showSlide(slideIndex + 1);
 });
@@ -51,13 +82,16 @@ bikefilters.forEach(element => {
   });
 });
 
-// function to set a given theme/color-scheme
+selectBikeOptions.addEventListener('change', (event) => {
+  filterBikes(event.target.value);
+  setFilterState(event.target.value, true);
+})
+
 function setTheme(themeName) {
   localStorage.setItem('theme', themeName);
   document.documentElement.className = themeName;
 }
 
-// function to toggle between light and dark theme
 function toggleTheme() {
   if (localStorage.getItem('theme') === 'theme-dark'){
     setTheme('theme-light');
@@ -66,7 +100,6 @@ function toggleTheme() {
   }
 }
 
-// Immediately invoked function to set the theme on initial load
 (function () {
   if (localStorage.getItem('theme') === 'theme-dark') {
     setTheme('theme-dark');
@@ -76,3 +109,15 @@ function toggleTheme() {
 })();
 
 themeSwitcher.addEventListener('click', toggleTheme);
+
+menuLinks.forEach(element => {
+  element.addEventListener('click', () => {
+    menuInput['checked'] = false;
+  })
+});
+
+bikeDots.forEach(element => {
+  element.addEventListener('click', () => {
+    showBikesSlide(element.getAttribute('data-id'), getCurrentTerrainSlides());
+  })
+})
